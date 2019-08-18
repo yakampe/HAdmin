572e8b7378a7a2df0ad4a87654362c68f783d4ad
yakampe <yanis.kampe.cv@gmail.com>
2019-08-18 11:37:40 +0100

    Starter dependencies

---
+++ HAdmin/src/main/java/co/uk/yktech/HAdminApplication.java
@@ -0,0 +1,13 @@
+package co.uk.yktech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HAdminApplication.class, args);
+	}
+
+}
---
+++ HAdmin/src/main/java/co/uk/yktech/ServletInitializer.java
@@ -0,0 +1,13 @@
+package co.uk.yktech;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HAdminApplication.class);
+	}
+
+}
